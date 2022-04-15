669e8773dd15766e59d5977271c428c264c63bb8
DeadlyKitten-spec <m.budnikov2000@gmail.com>
2022-04-15 12:12:10 +0300

    HomeWork 7

---
+++ src/main/java/HomeWork7/Cat.java
@@ -0,0 +1,29 @@
+package HomeWork7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.satiety = false;
+    }
+
+    public void eat(Plate plate) {
+        plate.decreaseFood(appetite, this);
+    }
+
+    public void setSatiety(boolean satiety) {
+        this.satiety = satiety;
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
